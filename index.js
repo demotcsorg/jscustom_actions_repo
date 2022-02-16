b57c2a5724d4a8e6159e5460b264e0ef64d21f76
Saurabh Rai <saurabhrai473@gmail.com>
2022-02-16 12:03:41 +0530

    My first action is ready

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
+  let x= core.getBooleanInput('greetbool');
+  console.log(`GreetBool is ${x}`);
+  core.setSecret('myPassword');
+  core.exportVariable('envVar', 'Val'); 
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
